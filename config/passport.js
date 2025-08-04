9fe125ba1c708cc93b1d55be111432dd734575ac
dylanyng <dylanwadeyoung@me.com>
2025-08-04 17:30:22 -0400

    Add google passport, begin writing strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,22 @@
 // All boiler plate beloe
 
 // const LocalStrategy = require("passport-local").Strategy; // passport-local removed
+// const mongoose = require("mongoose");
+// const User = require("../models/User");
+////
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
