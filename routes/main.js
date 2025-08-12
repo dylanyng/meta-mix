bfdcf89f114e0c087a58e12e4cac1e098b95e2ae
dylanyng <dylanyoung@hey.com>
2025-08-11 20:26:31 -0400

    Protect routes

--- routes/main.js
+++ routes/main.js
@@ -1,21 +1,16 @@
-// All boiler plate below
-
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/auth");
-const homeController = require("../controllers/home");
-// const postsController = require("../controllers/posts"); // Removed
-const { ensureAuth } = require("../middleware/auth");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Main Routes 
-router.get("/", homeController.getIndex);
-// router.get("/profile", ensureAuth, postsController.getProfile); // Removed
+router.get('/', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login'
+  })
+})
 
-//Routes for user login/signup
-// router.get("/login", authController.getLogin);
-// router.post("/login", authController.postLogin);
-// router.get("/logout", authController.logout);
-// router.get("/signup", authController.getSignup);
-// router.post("/signup", authController.postSignup);
+router.get('/dashboard', ensureAuth, (req, res) => {
+  console.log(req.user)
+  res.render('dashboard')
+})
 
 module.exports = router;
